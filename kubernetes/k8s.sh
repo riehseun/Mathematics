e7799f7b31f657d30da2015644de77a2a88c1234
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-06-06 20:02:08 -0400

    update

--- kubernetes/k8s.sh
+++ kubernetes/k8s.sh
@@ -7,3 +7,6 @@ kubectl cluster-info
 kubectl get service
 kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts:default
 kubectl -n kube-system get deployment coredns -o yaml | sed 's/allowPrivilegeEscalation: false/allowPrivilegeEscalation: true/g' | kubectl apply -f -
+
+cd ../jenkins
+# curl -s -XPOST 'http://?:30000/createItem?name=seed-job' --data-binary @seed-job.xml -H "Content-Type:text/xml"
