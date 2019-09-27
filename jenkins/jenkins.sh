b7cd81622582709bc2d9ae9387fc39913340ff80
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-09-26 21:13:56 -0400

    update

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -2,4 +2,5 @@
 
  POD=$(kubectl get pods | awk '{print $1}' | head -n 2)
  kubectl cp config.xml $POD:/var/jenkins_home/config.xml
+ kubectl cp org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml $POD:/var/jenkins_home/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml
  #kubectl exec -it $POD -- /bin/bash
