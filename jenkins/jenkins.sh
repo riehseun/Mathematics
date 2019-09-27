495b85487eca966c4de8ec5dbacd3178d6d2b379
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-09-26 21:26:20 -0400

    update

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
  POD=$(kubectl get pods | awk '{print $1}' | head -n 2)
+
+ # Copy local files into directories inside container
  kubectl cp config.xml $POD:/var/jenkins_home/config.xml
  kubectl cp org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml $POD:/var/jenkins_home/org.jenkinsci.plugins.workflow.libs.GlobalLibraries.xml
+ IP=$(kubectl get ingress | awk '{print $3}' | tail -n 1)
+ wget http://$IP/restart
  #kubectl exec -it $POD -- /bin/bash
