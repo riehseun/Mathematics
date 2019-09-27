7a33369ba7c11b3584694691f33fc1c22d3cdb03
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-09-26 21:52:06 -0400

    update

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- POD=$(kubectl get pods | awk '{print $1}' | head -n 2)
+ POD=$(kubectl get pods | awk '{print $1}' | tail -n 1)
 
  # Copy local files into directories inside container
  kubectl cp config.xml $POD:/var/jenkins_home/config.xml
