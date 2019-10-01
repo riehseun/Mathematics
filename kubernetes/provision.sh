348f0e6436318d72f2c28e07936b1a8b6bd3c450
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-09-30 22:50:55 -0400

    update

--- kubernetes/provision.sh
+++ kubernetes/provision.sh
@@ -18,9 +18,12 @@ gcloud container clusters create k8s
 # Reverve static IP for "jenkins-master" application
 gcloud compute addresses create jenkins-master --global
 
+# NGINX Ingress Controller setup
+# Initialize current user as a cluster-admin
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/mandatory.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud-generic.yaml
+# Verify installation
 kubectl get pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx --watch
 
 kubectl apply -f jenkins-deployment.yaml
