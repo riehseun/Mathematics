7a79a111321deede5d7eb70f4bde2b346cc6e48f
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-10-11 00:21:32 -0400

    update

--- kubernetes/test-provision.sh
+++ kubernetes/test-provision.sh
@@ -2,7 +2,7 @@
 gcloud container clusters create k8s
 
 # Reverve static IP for "jenkins-master" application
-gcloud compute addresses create web-static-ip --global
+#gcloud compute addresses create web-static-ip --global
 
 kubectl apply -f test-deployment.yaml
 kubectl apply -f test-service.yaml
