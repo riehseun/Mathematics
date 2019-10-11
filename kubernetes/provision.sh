e476c90aac441ac5ee103b2a4831c301b63fa19c
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-10-10 23:44:40 -0400

    update

--- kubernetes/provision.sh
+++ kubernetes/provision.sh
@@ -16,7 +16,7 @@
 gcloud container clusters create k8s
 
 # Reverve static IP for "jenkins-master" application
-gcloud compute addresses create jenkins-master --global
+# gcloud compute addresses create jenkins-master --global
 
 # NGINX Ingress Controller setup
 # Initialize current user as a cluster-admin
