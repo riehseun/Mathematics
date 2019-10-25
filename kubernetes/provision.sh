3daf7362d6f3a0b1d5201c9f2c0290e76aa3545f
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-10-24 22:56:18 -0400

    update

--- kubernetes/provision.sh
+++ kubernetes/provision.sh
@@ -26,6 +26,7 @@ gcloud compute addresses create jenkins-master --global
 # Verify installation
 # kubectl get pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx --watch
 
+kubectl apply -f jenkins-backendservice.yaml
 kubectl apply -f jenkins-ingress.yaml
 kubectl apply -f jenkins-service.yaml
 kubectl apply -f jenkins-deployment.yaml
