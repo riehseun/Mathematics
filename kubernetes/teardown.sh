28b598810972c9e143acd2382b780b2b2820232b
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-11-05 20:21:45 -0500

    update

--- kubernetes/teardown.sh
+++ kubernetes/teardown.sh
@@ -3,12 +3,12 @@
 # Remove all backend services
 BS=$(gcloud compute backend-services list | awk '{print $1}' | tail -n 2)
 for i in $BS; do
-	gcloud -q compute backend-services delete $i
+	gcloud -q compute backend-services delete $i --global
 done
 
 # Remove forwarding rules
 FR=$(gcloud compute forwarding-rules list | awk '{print $1}' | tail -n 1)
-gcloud -q compute backend-services delete $FR
+gcloud -q compute backend-services delete $FR --global
 
 # Remove static IP
 gcloud -q compute addresses delete jenkins-master --global
@@ -16,12 +16,12 @@ gcloud -q compute addresses delete jenkins-master --global
 # Remove health checks
 HC=$(gcloud compute health-checks list | awk '{print $1}' | tail -n 2)
 for i in $HC; do
-	gcloud -q compute health-checks delete $HC
+	gcloud -q compute health-checks delete $HC --global
 done
 
 # Remove url maps
 UM=$(gcloud compute url-maps list | awk '{print $1}' | tail -n 1)
-gcloud compute url-maps delete $UM
+gcloud -q compute url-maps delete $UM --global
 
 # Remove ingress
 kubectl delete ingress jenkins-ingress
