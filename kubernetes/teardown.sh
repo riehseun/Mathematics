715c1573944eace0c8bba50b5275b1ba85eced0a
Seungmoon Rieh <seungmoon.rieh@gmail.com>
2019-11-05 20:11:38 -0500

    update

--- kubernetes/teardown.sh
+++ kubernetes/teardown.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-kubectl delete ingress jenkins-ingress
 
-gcloud -q compute addresses delete jenkins-master --global
+LB=$(gcloud compute url-maps list | awk '{print $1}' | tail -n 1)
+echo $LB
 
-gcloud -q container clusters delete k8s
+BS=$(gcloud compute backend-services list | awk '{print $1}' | tail -n 2)
+
+for i in $BS
+	echo $i
+end
+
+# gcloud compute backend-services delete $BS
+
+
+
+#gcloud -q compute addresses delete jenkins-master --global
+
+#kubectl delete ingress jenkins-ingress
+
+#gcloud -q container clusters delete k8s
